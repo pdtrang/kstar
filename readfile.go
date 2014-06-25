be65369c407f56b98938bdb69619fccfe498437f
Diem-Trang Pham <phamdiemtrang1987@gmail.com>
2014-06-25 15:19:51 -0500

    Update readfile.go

--- readfile.go
+++ readfile.go
@@ -5,7 +5,42 @@ import (
     "fmt"
     "bufio"
     "bytes"
+    "log"
 )
+
+func readLines(path string) ([]string, error) {
+  file, err := os.Open(path)
+  if err != nil {
+    return nil, err
+  }
+  defer file.Close()
+
+  var lines []string
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+  return lines, scanner.Err()
+}
+
+func ReadVCF(vcf_file string) []string{
+    lines, err := readLines(vcf_file)
+    if err != nil {
+        log.Fatalf("readLines: %s", err)
+    }
+    //for i, line := range lines {
+    //    fmt.Println(i, line)
+    //}
+
+    //fmt.Println("read VCF")
+    //fmt.Println(lines)
+    //for i := 0; i < len(lines); i++ {
+    //    fmt.Println(lines[i])
+    //}
+
+    
+    return lines
+}
 
 func ReadFASTA(sequence_file string) []byte {
     f,err := os.Open(sequence_file)
