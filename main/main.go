0bb755243808c1a330fdabe7ec9291ac0b9fb176
Diem-Trang Pham <phamdiemtrang1987@gmail.com>
2014-06-30 10:57:06 -0500

    Update main.go

--- main/main.go
+++ main/main.go
@@ -4,8 +4,6 @@ import (
     "fmt"
     "flag"
     "readfile"
-    "strings"
-    "strconv"
     "kstar"
 )
 
@@ -15,36 +13,17 @@ func main () {
     flag.Parse()
 
     sequence := readfile.ReadFASTA(*genome_file)   
-    snp_f := readfile.ReadVCF(*snp_file)
+    dbsnp := readfile.ReadVCF(*snp_file)
     
     fmt.Println(string(sequence))
-    //fmt.Println(len(sequence))
-
-    /*fmt.Println("print in main")
-    fmt.Println(len(snp_f))
-    for i := 0; i < len(snp_f); i++ {
-        fmt.Println(snp_f[i])
-    }*/
-
-    pos := make([]int, len(snp_f))
-    snp_a := make([][]string, len(snp_f))
-    for i := 0; i < len(snp_f); i++ {
-    	a := strings.Split(string(snp_f[i]), "\t")	
-    	pos[i],_ = strconv.Atoi(a[0])
-    	snp_a[i] = strings.Split(a[1], "/")
-    }
-
+    fmt.Println(len(sequence))
     
-    //for i := 0; i < len(snp_f); i++ {
-    //	fmt.Println("snp pos ", pos[i], "snp ", snp_a[i])	
-    //}
-
     var i kstar.Index
    	i = *kstar.NewIndex([]byte(sequence))
 
-    fmt.Println("\naddSNP")
-   	for j := 0; j < len(snp_f); j++ {
-   		i.AddSNP([]byte(sequence), snp_a[j], pos[j])	
+   	for j := range dbsnp {
+        snp := dbsnp[j].GetString()
+    	i.AddSNP([]byte(sequence), snp[1:], j)	
    	}
    	
     fmt.Println(i)
